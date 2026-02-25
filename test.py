1c044c8eb247aba9bae87f318cd1117266b8dfde
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-25 13:36:42 +0500

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from instagpy import InstaGPy
-insta = InstaGPy(use_mutiple_account=False, session_ids=None, min_requests=None, max_requests=None)
-txt = insta.get_user_basic_details("abdulloh_martialarts")
-print(txt)
